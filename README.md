18dfe8e49a78cd076615041feffab9082a487f22
Jake Craige <jake@poeticsystems.com>
2014-05-07 23:01:07 -0500

    update locationType in environment config after install

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ must also run `ember server` at the same time so that the files get recompiled
 
 `script/symlink_prod.js` to set up cordova symlink to watch dist directory.
 
+# important info
+
+locationType must be set to hash!
+
 # Usage
 
 work out of app/ directory
--- install.js
+++ install.js
@@ -35,7 +35,8 @@ var cordovaCommand = 'cordova create app ' + reverseDomain + ' ' + nameOfApp
 
 var cdvAppPath = path.join(process.cwd(), 'app');
 
-var emberDistPath = path.join(cdvAppPath, 'ember', 'dist');
+var emberPath = path.join(cdvAppPath, 'ember');
+var emberDistPath = path.join(emberPath, 'dist');
 var wwwPath       = path.join(cdvAppPath, 'www');
 
 var symlinkCommand = 'rm -r ' + wwwPath + '; ln -s ' + emberDistPath + ' ' + wwwPath;
@@ -46,24 +47,37 @@ var commands = [
   runCommand(symlinkCommand)
 ];
 
+function copyTemplates() {
+  var beforeBuildPath =path.join(cdvAppPath, 'hooks', 'before_build');
+  var fileName = '001_build_ember_app.js';
+
+  fs.mkdirSync(beforeBuildPath);
+  fs.writeFileSync(
+    path.join(beforeBuildPath, fileName),
+    fs.readFileSync(path.join(process.cwd(), 'templates', fileName)),
+    {
+      mode: "755"
+    }
+  );
+  console.log('done creating templates');
+}
+
+function updateEnvConfig() {
+  var envPath = path.join(emberPath, 'config', 'environment.js');
+  var env = fs.readFileSync(envPath, { encoding: 'utf8' })
+  env = env.replace("locationType: 'auto'", "locationType: 'hash'");
+  fs.writeFileSync(envPath, env);
+  console.log('changed config locationType to hash');
+}
+
 async.series(commands,
   function(err) {
     if(err) throw err;
 
     console.log('Created basic structure..');
+    copyTemplates();
 
-    var beforeBuildPath =path.join(cdvAppPath, 'hooks', 'before_build');
-    var fileName = '001_build_ember_app.js';
-
-    fs.mkdirSync(beforeBuildPath);
-    fs.writeFileSync(
-      path.join(beforeBuildPath, fileName),
-      fs.readFileSync(path.join(process.cwd(), 'templates', fileName)),
-      {
-        mode: "755"
-      }
-    );
-    console.log('done creating templates');
+    updateEnvConfig();
 
   }
 );
