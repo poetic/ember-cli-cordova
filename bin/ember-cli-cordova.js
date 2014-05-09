fd453c5da22c43ebb2856ab31c332291dc17d550
Jake Craige <jake@poeticsystems.com>
2014-05-08 19:43:59 -0500

    major organizational refactor. change bin command to ember-cdv, add .ember-cdv config file

--- bin/ember-cli-cordova.js
+++ bin/ember-cli-cordova.js
@@ -6,16 +6,14 @@ var chalk   = require('chalk');
 var options = require('minimist')(process.argv.slice(2));
 
 var command     = options._[0];
-var commandPath = path.join(__dirname, 'lib', 'commands', command + '.js');
-
-console.log(chalk.cyan('::Ember CLI Cordova::'));
+var commandPath = path.join(__dirname, '..', 'lib', 'commands', command + '.js');
 
 fs.exists(commandPath, function(exists){
   if(exists) {
-    require('./lib/commands/' + command)(options);
+    require('../lib/commands/' + command)(options);
   } else {
     console.log(chalk.red.underline("Error: Unknown command"));
-    require('./lib/commands/help')();
+    require('../lib/commands/help')();
   }
 });
 
