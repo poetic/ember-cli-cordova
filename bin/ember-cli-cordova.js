8724858567b7fd027c223878923ee2755fe020f5
Jake Craige <jake@poeticsystems.com>
2014-05-09 08:38:26 -0500

    set up the commands to use a similar Command prototype that provides similar functionality to each command and makes adding them easier.

--- bin/ember-cli-cordova.js
+++ bin/ember-cli-cordova.js
@@ -10,7 +10,14 @@ var commandPath = path.join(__dirname, '..', 'lib', 'commands', command + '.js')
 
 fs.exists(commandPath, function(exists){
   if(exists) {
-    require('../lib/commands/' + command)(options);
+    if(command === 'help') {
+      require('../lib/commands/help')();
+    } else if(options._[1] === 'help' || options.h) {
+      require('../lib/commands/' + command)(options).displayUsage();
+    }
+    else {
+      require('../lib/commands/' + command)(options).validateAndRun();
+    }
   } else {
     console.log(chalk.red.underline("Error: Unknown command"));
     require('../lib/commands/help')();
