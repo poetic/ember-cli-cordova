890fc00e85023c018d885bfbc97496320e501c8a
Jake Craige <jake@poeticsystems.com>
2014-05-22 08:51:01 -0500

    add version output when running command, thanks ember-cli :)

--- bin/ember-cli-cordova.js
+++ bin/ember-cli-cordova.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
-var fs      = require('fs');
-var path    = require('path');
-var chalk   = require('chalk');
-var options = require('minimist')(process.argv.slice(2));
+var fs              = require('fs');
+var path            = require('path');
+var chalk           = require('chalk');
+var options         = require('minimist')(process.argv.slice(2));
+var emberCDVVersion = require('../lib/utils/ember-cdv-version');
 
 var userCommand     = options._[0];
 var userCommandPath = path.join(__dirname, '..', 'lib', 'commands', userCommand + '.js');
 var command;
 
+console.log('version:', emberCDVVersion(), '\n');
 fs.exists(userCommandPath, function(exists){
   if(exists) {
     command = new (require('../lib/commands/' + userCommand))(options)
