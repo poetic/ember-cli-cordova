50d382d529656c2c7010d23b926f7ea2f4b30133
Jake Craige <jake@poeticsystems.com>
2014-05-27 20:04:25 -0500

    The big console.log to ui.write change, closes #11

--- bin/ember-cli-cordova.js
+++ bin/ember-cli-cordova.js
@@ -5,25 +5,26 @@ var path            = require('path');
 var chalk           = require('chalk');
 var options         = require('minimist')(process.argv.slice(2));
 var emberCDVVersion = require('../lib/utils/ember-cdv-version');
+var ui              = require('../lib/ui');
 
 var userCommand     = options._[0];
-var userCommandPath = path.join(__dirname, '..', 'lib', 'commands', userCommand + '.js');
+var userCommandPath = path.join(__dirname, '../lib/commands', userCommand + '.js');
 var command;
 
-console.log('version:', emberCDVVersion(), '\n');
+ui.write('version: ' + emberCDVVersion() + '\n\n');
 fs.exists(userCommandPath, function(exists){
   if(exists) {
     command = new (require('../lib/commands/' + userCommand))(options)
 
     if(options._[1] === 'help' || options.h) {
       command.displayHelp();
-      console.log();
+      ui.write('\n');
     }
     else {
       command.validateAndRun();
     }
   } else {
-    console.log(chalk.red.underline("Error: Unknown command"));
+    ui.write(chalk.red.underline("Error: Unknown command"));
     var command = new (require('../lib/commands/help'))();
     command.validateAndRun();
   }
