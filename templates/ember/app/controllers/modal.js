d87627adf2058f55b34394cf0ca1b8c789fa123e
Jake Craige <jake@poeticsystems.com>
2014-07-04 18:29:21 -0500

    remove lots of extra files from the init command. Update them to be pretty minimal. Update environment config to have defaults for rebuild

--- templates/ember/app/controllers/modal.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.ObjectController.extend({
-  actions: {
-    close: function() {
-      this.send('closeModal');
-    }
-  }
-});
--- templates/ember/app/routes/application.js
+++ templates/ember/app/routes/application.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.extend({
   actions: {
     back: function() {
       Ember.AnimatedContainerView.enqueueAnimations({main: 'slideRight'});
