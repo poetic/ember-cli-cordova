d5a2250efa8edc07efb0b1a18b23f7e2652fc251
Jake Craige <jake@poeticsystems.com>
2014-07-17 18:16:00 -0500

    add proxy quire, it's awesome. And add-platform command test

---
+++ tests/unit/tasks/add-platforms-test.js
@@ -0,0 +1,24 @@
+var proxyquire = require('proxyquire');
+var expect     = require('chai').expect;
+
+describe('Tasks - Add Platforms', function() {
+  it('creates the proper command', function() {
+    var addPlatforms = proxyquire('../../../lib/tasks/add-platforms', {
+      '../utils/run-command': function(command) {
+        expect(command).to.contain('platforms add some-platform');
+      }
+    });
+
+    addPlatforms({root: 'test'}, {platform: 'some-platform'});
+  });
+
+  it('executes command in cordova directory', function() {
+    var addPlatforms = proxyquire('../../../lib/tasks/add-platforms', {
+      '../utils/run-command': function(_, __, options) {
+        expect(options.cwd).to.equal('test/cordova');
+      }
+    });
+
+    addPlatforms({root: 'test'}, {platform: 'some-platform'});
+  });
+});
