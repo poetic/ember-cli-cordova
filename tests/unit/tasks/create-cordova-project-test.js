58658ca2b511df41602b349807ecf6b302a719f3
Jake Craige <jake@poeticsystems.com>
2014-07-17 19:22:56 -0500

    create-cordova-project test

---
+++ tests/unit/tasks/create-cordova-project-test.js
@@ -0,0 +1,35 @@
+var Config = require('../../../lib/utils/config');
+
+describe('Tasks - Create cordova project', function() {
+  var project;
+
+  beforeEach(function() {
+    project = {
+      cdvConfig: new Config('/path/that/doesnt/exist', {
+        id: 'com.poetic.test-app',
+        name: 'TestApp'
+      }),
+      root: 'project-root'
+    };
+  });
+
+  it('creates the proper command', function() {
+    var createProject = proxyquire('../../lib/tasks/create-cordova-project', {
+      '../utils/run-command': function(command) {
+        expect(command).to.contain('create cordova com.poetic.test-app TestApp');
+      }
+    });
+
+    createProject(project);
+  });
+
+  it('should execute in proper folder', function() {
+    var createProject = proxyquire('../../lib/tasks/create-cordova-project', {
+      '../utils/run-command': function(_, __, options) {
+        expect(options.cwd).to.equal('project-root');
+      }
+    });
+
+    createProject(project);
+  });
+});
