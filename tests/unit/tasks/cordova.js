f0b02e9e2ba21143fa61d132f61d78fdaf05880b
Jake Craige <jake@poeticsystems.com>
2014-07-17 19:50:21 -0500

    add test for cordova task

---
+++ tests/unit/tasks/cordova.js
@@ -0,0 +1,25 @@
+var noop = require('../../helpers/noop');
+
+describe('Tasks - Cordova', function() {
+  it('creates a proper command', function() {
+    var cordova = proxyquire('../../lib/tasks/cordova', {
+      '../utils/run-command': function(command, msg, options){
+        expect(command).to.match(/cordova plugins add org\.apache\.test$/);
+        return noop;
+      },
+    });
+
+    cordova(['plugins', 'add', 'org.apache.test'], { root: 'test' })();
+  });
+
+  it('executes in proper directory', function() {
+    var cordova = proxyquire('../../lib/tasks/cordova', {
+      '../utils/run-command': function(command, msg, options){
+        expect(options.cwd).to.match(/test\/cordova$/);
+        return noop;
+      },
+    });
+
+    cordova(['plugins', 'add', 'org.apache.test'], { root: 'test' })();
+  });
+});
