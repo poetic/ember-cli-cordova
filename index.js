6c8e0fe2ecfd6675a42946bc1b570542ed6225db
Jake Craige <jake@poeticsystems.com>
2014-09-15 21:36:26 -0500

    modern addon file

--- index.js
+++ index.js
@@ -4,34 +4,32 @@ var path      = require('path');
 var commands  = require('./lib/commands');
 var postBuild = require('./lib/tasks/post-build');
 
-function EmberCLICordova(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Cordova';
-  this.setConfig();
-}
+module.exports = {
+  name: 'ember-cli-cordova',
+  init: function() {
+    this.setConfig();
+  },
 
-EmberCLICordova.prototype.blueprintsPath = function() {
-  return path.join(__dirname, 'blueprints');
-}
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
 
-EmberCLICordova.prototype.includedCommands = function() {
-  return commands;
-}
+  includedCommands: function() {
+    return commands;
+  },
 
-EmberCLICordova.prototype.setConfig = function(){
-  var env = this.project.config('development');
+  setConfig: function(){
+    var env = this.project.config('development');
+    if(!this.config) {
+      this.config = {};
+    }
 
-  if(!this.config) {
-    this.config = {};
+    if (env.cordova) {
+      this.config = env.cordova;
+    }
+  },
+
+  postBuild: function() {
+    return postBuild(this.project, this.config)();
   }
-
-  if (env.cordova) {
-    this.config = env.cordova;
-  }
-}
-
-EmberCLICordova.prototype.postBuild = function() {
-  return postBuild(this.project, this.config)();
-}
-
-module.exports = EmberCLICordova;
+};
