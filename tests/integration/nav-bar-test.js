87d6bf9a2b09aff5155d855cf75b0d1b913db2ae
Jake Craige <jake@poeticsystems.com>
2014-10-13 07:48:50 -0500

    test actions

--- tests/integration/nav-bar-test.js
+++ tests/integration/nav-bar-test.js
@@ -2,21 +2,77 @@ import describeApp from '../helpers/describe-app';
 
 describeApp('Integration - Nav Bar', function() {
   describe('Route Mixin', function() {
-    it('updates template', function() {
-      visit('nav-bar');
+    describe('template', function() {
+      it('updates between routes', function() {
+        visit('nav-bar');
 
-      andThen(function() {
-        equal(find('header h1').text().trim(), 'Index');
-        equal(find('header button:first').text().trim(), 'iLeft');
-        equal(find('header button:last').text().trim(), 'iRight');
+        andThen(function() {
+          equal(find('header h1').text().trim(), 'Index');
+          equal(find('header button:first').text().trim(), 'iLeft');
+          equal(find('header button:last').text().trim(), 'iRight');
+        });
+
+        visit('nav-bar/page-1');
+
+        andThen(function() {
+          equal(find('header h1').text().trim(), 'Page 1');
+          equal(find('header button:first').text().trim(), 'pLeft');
+          equal(find('header button:last').text().trim(), 'pRight');
+        });
+      });
+    });
+
+    describe('actions', function() {
+      before(function() {
+        var navIndex = this.app.__container__.lookup('route:nav-bar/index');
+        var page1    = this.app.__container__.lookup('route:nav-bar/page-1');
+        var spec     = this;
+
+        spec.navIndexStub = sinon.stub();
+        navIndex.set('nav.actions', {
+          leftButton: function() { spec.navIndexStub('left'); },
+          rightButton: function() { spec.navIndexStub('right'); }
+        });
+
+        spec.page1Stub = sinon.stub();
+        page1.set('nav.actions', {
+          leftButton: function() { spec.page1Stub('left'); },
+          rightButton: function() { spec.page1Stub('right'); }
+        });
       });
 
-      visit('nav-bar/page-1');
+      it('updates between routes', function() {
+        visit('nav-bar');
 
-      andThen(function() {
-        equal(find('header h1').text().trim(), 'Page 1');
-        equal(find('header button:first').text().trim(), 'pLeft');
-        equal(find('header button:last').text().trim(), 'pRight');
+        click('header button:first');
+
+        andThen(function() {
+          ok(this.navIndexStub.calledWith('left'));
+          ok(!this.navIndexStub.calledWith('right'));
+        }.bind(this));
+
+        click('header button:last');
+
+        andThen(function() {
+          ok(this.navIndexStub.calledWith('left'));
+          ok(this.navIndexStub.calledWith('right'));
+        }.bind(this));
+
+        visit('nav-bar/page-1');
+
+        click('header button:first');
+
+        andThen(function() {
+          ok(this.page1Stub.calledWith('left'));
+          ok(!this.page1Stub.calledWith('right'));
+        }.bind(this));
+
+        click('header button:last');
+
+        andThen(function() {
+          ok(this.page1Stub.calledWith('left'));
+          ok(this.page1Stub.calledWith('right'));
+        }.bind(this));
       });
     });
   });
