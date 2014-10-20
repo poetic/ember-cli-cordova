1ebe90cc3db6431ec14568bcde265a9ca33d24c5
Jake Craige <jake@poeticsystems.com>
2014-10-20 14:02:33 -0500

    jshint fix

--- addon/initializers/in-app-livereload.js
+++ addon/initializers/in-app-livereload.js
@@ -1,6 +1,4 @@
-import deferReadiness from '../utils/defer-readiness';
 import redirect from '../utils/redirect';
-import startProxy from '../utils/start-proxy-server';
 
 export var initialize = function(container, app, config) {
   var url = config.cordova.emberUrl || 'http://localhost:4200';
