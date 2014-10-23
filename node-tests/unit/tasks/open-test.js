19074d794b7a86f4694b583608cea030eddff8de
Jake Craige <jake@poeticsystems.com>
2014-10-23 15:34:35 -0500

    per platform testing of open command and reject unsupported platform

--- node-tests/unit/tasks/open-test.js
+++ node-tests/unit/tasks/open-test.js
@@ -4,22 +4,73 @@ describe('Tasks - Open', function() {
     project = newProject();
   });
 
-  describe('runs correct command', function() {
-    it('ios', function() {
-      var open = proxyquire('../../lib/tasks/open', {
-        '../utils/run-command': function(command) {
-          expect(command).to.eql('xdg-open "project-root/cordova/platforms/ios/*.xcodeproj"');
-        }
-      });
-      return open(project,'ios');
+  it('rejects when the platform isn\'t supported', function() {
+    var open = proxyquire('../../lib/tasks/open', {});
+
+    return open(project, 'fake-platform').catch(function(err) {
+      expect(err.toString()).to.match(/platform is not supported/);
     });
-    it('android', function() {
-      var open = proxyquire('../../lib/tasks/open', {
-        '../utils/run-command': function(command) {
-          expect(command).to.eql('xdg-open "project-root/cordova/platforms/android/.project"');
-        }
+  });
+
+  describe('runs correct command on each platform', function() {
+    var platform;
+    before(function() {
+      platform = process.platform;
+    });
+
+    after(function() {
+      process.platform = platform;
+    });
+
+    describe('darwin', function() {
+      beforeEach(function() {
+        process.platform = 'darwin';
       });
-      return open(project,'android');
+
+      it('ios', function() {
+        return assertOpenCommand(project, 'ios', 'open "project-root/cordova/platforms/ios/*.xcodeproj"');
+      });
+
+      it('android', function() {
+        return assertOpenCommand(project, 'android', 'open "project-root/cordova/platforms/android/.project"');
+      });
+    });
+
+    describe('win32', function() {
+      beforeEach(function() {
+        process.platform = 'win32';
+      });
+
+      it('ios', function() {
+        return assertOpenCommand(project, 'ios', 'start "project-root/cordova/platforms/ios/*.xcodeproj"');
+      });
+
+      it('android', function() {
+        return assertOpenCommand(project, 'android', 'start "project-root/cordova/platforms/android/.project"');
+      });
+    });
+
+    describe('other', function() {
+      beforeEach(function() {
+        process.platform = 'other';
+      });
+
+      it('ios', function() {
+        return assertOpenCommand(project, 'ios', 'xdg-open "project-root/cordova/platforms/ios/*.xcodeproj"');
+      });
+
+      it('android', function() {
+        return assertOpenCommand(project, 'android', 'xdg-open "project-root/cordova/platforms/android/.project"');
+      });
     });
   });
 });
+
+function assertOpenCommand(project, platform, assertion) {
+  var open = proxyquire('../../lib/tasks/open', {
+    '../utils/run-command': function(command) {
+      expect(command).to.eql(assertion);
+    }
+  });
+  return open(project, platform);
+}
